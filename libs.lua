d1644c22d2e612c5ee853139cb16b57010bd4af7
GameSTALkER <52292220+GameSTALkER@users.noreply.github.com>
2023-02-11 17:30:20 +0100

    1.01

--- libs.lua
+++ libs.lua
@@ -3,12 +3,17 @@ local repo = "NGSTREBORN"
 local path = "libs/"
 
 local github_api = loadstring(game:HttpGet("https://raw.githubusercontent.com/GameSTALkER/NGSTREBORN/main/libs/github-api.lua"))()
+local cached_libs = {}
 
 local function loader(lib_name)
     local response = github_api.Repo(author, repo, path .. tostring(lib_name))
     
-    if response and response.download_url then
-        return loadstring(game:HttpGet(response.download_url))
+    if cached_libs[tostring(lib_name)] then
+        return cached_libs[tostring(lib_name)]
+    
+    elseif response and response.download_url then
+        cached_libs[tostring(lib_name)] = loadstring(game:HttpGet(response.download_url))
+        return cached_libs[tostring(lib_name)]
         
     else
         return false, "Not found."
